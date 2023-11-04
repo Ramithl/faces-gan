c1a4ba6a52b300282e19fafd974f50b3b6004f94
Ramithl <ramith.laksara@gmail.com>
2023-11-04 19:34:44 +0530

    Layout done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="row">
+      <div class="column"><ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button>Blonde</Button>
+        <Button>Black</Button>
+      </ButtonGroup></div>
+      <div class="column">afaf</div>
     </div>
   );
 }
